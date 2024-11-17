ae3e4c532f029ae6abccc445eed316f3cb8ccbf4
Gusttavo06 <jggholderbaum01@gmail.com>
2024-11-17 15:22:28 -0300

    ADS-init: 01703455 Reactjs-vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/**
+ * Nome do arquivo: App.jsx
+ * Data de criação: 17/11/2024
+ * Autor: João Gustavo Gomes Holderbaum
+ * Matrícula: 01703455
+ *
+ * Descrição:
+ * Este arquivo é o componente principal da aplicação de Dashboard de Criptomoedas.
+ * Ele renderiza a interface principal, incluindo o cabeçalho e o conteúdo da página.
+ */
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
